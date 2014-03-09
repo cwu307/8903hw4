567694c769daef29474273b3644bd75a5c9b5ba9
royzxq <xinquanzhou@lawn-128-61-127-183.lawn.gatech.edu>
2014-03-08 22:33:31 -0500

    tmp change

---
+++ MyProject/src/MyProject/MyProject.cpp
@@ -0,0 +1,171 @@
+
+// standard headers
+
+// project headers
+#include "MyProjectConfig.h"
+
+#include "ErrorDef.h"
+
+#include "MyProject.h"
+
+static const char*  kCMyProjectBuildDate             = __DATE__;
+
+
+CMyProject::CMyProject ()
+{
+    // this never hurts
+    this->resetInstance ();
+}
+
+
+CMyProject::~CMyProject ()
+{
+    this->resetInstance ();
+}
+
+const int  CMyProject::getVersion (const Version_t eVersionIdx)
+{
+    int iVersion = 0;
+
+    switch (eVersionIdx)
+    {
+    case kMajor:
+        iVersion    = MyProject_VERSION_MAJOR; 
+        break;
+    case kMinor:
+        iVersion    = MyProject_VERSION_MINOR; 
+        break;
+    case kPatch:
+        iVersion    = MyProject_VERSION_PATCH; 
+        break;
+    case kNumVersionInts:
+        iVersion    = -1;
+        break;
+    }
+
+    return iVersion;
+}
+const char*  CMyProject::getBuildDate ()
+{
+    return kCMyProjectBuildDate;
+}
+
+Error_t CMyProject::createInstance (CMyProject*& pCMyProject)
+{
+    pCMyProject = new CMyProject ();
+
+    if (!pCMyProject)
+        return kUnknownError;
+
+
+    return kNoError;
+}
+
+Error_t CMyProject::destroyInstance (CMyProject*& pCMyProject)
+{
+    if (!pCMyProject)
+        return kUnknownError;
+    
+    pCMyProject->resetInstance ();
+    
+    delete pCMyProject;
+    pCMyProject = 0;
+
+    return kNoError;
+
+}
+
+Error_t CMyProject::initInstance()
+{
+    // allocate memory
+
+    // initialize variables and buffers
+
+    return kNoError;
+}
+
+Error_t CMyProject::resetInstance ()
+{
+    // reset buffers and variables to default values
+
+    return kNoError;
+}
+
+void LFO::setPara(float amp,float freq, float samplerate)
+{
+    Amp = amp;
+    Freq = freq;
+    SampleRate = samplerate;
+    time = 0 ;
+    
+}
+int vibrato::round(float x)
+{
+    int n = static_cast<int>(x);
+    if ((x-n)<0.5) {
+        return n;
+    }
+    else return n+1;
+}
+void vibrato::setParameter(float d,float Mf, float w){
+    Modfreq = Mf/samplerate;
+    if (w>MAX_BUFFER) {
+        w=MAX_BUFFER;
+    }
+    WIDTH = round(w * samplerate);
+    if (d>MAX_BUFFER) {
+        d=MAX_BUFFER;
+    }
+    Delay = d;
+    DELAY = round(Delay*samplerate);
+    
+    sinwav->setPara(WIDTH, Modfreq, samplerate);
+    
+    
+    
+}
+void vibrato::resetInstance()
+{
+    for (int i = 0 ; i < iNumChannel; i++) {
+        Delay_buffer[i]->resetInstance();
+        
+    }
+    sinwav->reset();
+    
+}
+
+void vibrato::process(float **input, float **output, int len)
+{
+    for (int n = 0 ; n<len; n++) {
+        float tap = DELAY+sinwav->getvalue();
+        for (int i = 0 ; i < iNumChannel; i++) {
+            Delay_buffer[i]->setDelayIdx(Delay_buffer[i]->getWriteIdx()-tap);
+            Delay_buffer[i]->putPostInc(input[i][n]);
+            output[i][n] = Delay_buffer[i]->getInterpolationPostInc();
+        }
+        
+    }
+}
+void vibrato::processBypass(float **input, float **output, int len)
+{
+    setParameter(Delay, 0, 0);
+    process(input, output, len);
+}
+
+void PPM::process(float *x, float *output)
+{
+        for (int i = 0 ; i < iNumChannel; i++) {
+            if (fabs(x[i])<vppm_old[i]) {
+                output[i] = (1-alfa1)*vppm_old[i];
+                
+            }
+            else{
+                output[i] = alfa2*fabs(x[i]) + (1-alfa2)*vppm_old[i];
+            }
+            vppm_old[i] =  output[i];
+        
+        }
+
+    
+    
+}
