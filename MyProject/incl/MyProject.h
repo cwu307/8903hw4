795a5395637887cc392b3acbf72f95d81d3c8cb8
royzxq <xinquanzhou@Xinquans-MacBook-Pro.local>
2014-04-08 01:39:42 -0400

    Unfinish job in integrating the three parts

--- MyProject/incl/MyProject.h
+++ MyProject/incl/MyProject.h
@@ -2,6 +2,9 @@
 #define __MyProject_hdr__
 
 #include "ErrorDef.h"
+#include "InputBuffSrc.h"
+#include "../../SharedSources/Dsp/Fft.h"
+#include "FeatureExtractor.h"
 
 class CMyProject
 {
@@ -22,14 +25,26 @@ public:
     static Error_t createInstance (CMyProject*& pCKortIf);
     static Error_t destroyInstance (CMyProject*& pCKortIf);
     
-    Error_t initInstance (/*enter parameters here*/);
+    Error_t initInstance (int NumFFT, int Blocksize, float SampleRate, int NumChannel=1);
     Error_t resetInstance ();
     
-    //virtual Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames) = 0;
-
+    virtual Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames);
+    
 protected:
     CMyProject ();
     virtual ~CMyProject ();
+    
+    CInputBuffSrc<float> * MyInputBuff;
+    CFft ** MyFFT;
+    FeatureExtractor ** MyFeatureExtractor;
+    float ** OutputBuffer;
+    CFft::complex_t ** Spect;
+    float ** SpectMagnit ; 
+    
+    int iNumChannal;
+    int iNumFFT;
+    int iBlocksize;
+    
 };
 
 #endif // #if !defined(__MyProject_hdr__)
--- MyProject/src/MyProject/MyProject.cpp
+++ MyProject/src/MyProject/MyProject.cpp
@@ -75,18 +75,52 @@ Error_t CMyProject::destroyInstance (CMyProject*& pCMyProject)
 
 }
 
-Error_t CMyProject::initInstance()
+Error_t CMyProject::initInstance(int NumFFT, int Blocksize, float SampleRate, int NumChannel)
 {
     // allocate memory
-
+    OutputBuffer = new float*[NumChannel];
+    Spect = new CFft::complex_t*[NumChannel];
+    SpectMagnit = new float*[NumChannel];
     // initialize variables and buffers
-
+    
+    iNumChannal = NumChannel;
+    iNumFFT = NumFFT;
+    iBlocksize = Blocksize;
+    
+    MyFFT = new CFft*[NumChannel];
+    MyFeatureExtractor = new FeatureExtractor*[NumChannel];
+    MyInputBuff = new CInputBuffSrc<float>(iNumChannal,Blocksize*2,iNumFFT-1);
+    
+    for (int i = 0 ;  i < NumChannel; i++) {
+        CFft::createInstance(MyFFT[i]);
+        MyFFT[i] -> initInstance(Blocksize,NumFFT);
+        MyFeatureExtractor[i] = new FeatureExtractor(SampleRate,Blocksize);
+        OutputBuffer[i] = new float[Blocksize];
+        Spect[i] = new CFft::complex_t[iNumFFT];
+        SpectMagnit[i] = new float[iNumFFT];
+    }
     return kNoError;
 }
 
 Error_t CMyProject::resetInstance ()
 {
     // reset buffers and variables to default values
+    for (int i = 0 ; i < iNumChannal; i++) {
+        MyFFT[i] -> resetInstance();
+        MyFeatureExtractor[i] -> reset();
+    }
+    MyInputBuff -> reset();
 
     return kNoError;
+}
+
+Error_t CMyProject::process(float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames)
+{
+    MyInputBuff -> setDataPtr2Hold(ppfInputBuffer, iNumberOfFrames);
+    while (MyInputBuff -> getBlock(OutputBuffer, iNumFFT)) {
+        for (int i = 0 ; i < iNumChannal ; i++) {
+            MyFFT[i] -> doFft(Spect[i], OutputBuffer[i]);
+            
+        }
+    }
 }
